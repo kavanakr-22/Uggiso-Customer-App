a3472607ebe6a49e7dda381e02e5b828b3fe9abc
Tilakrk23 <tilak@innoventes.co>
2024-04-02 22:22:34 +0530

    Signup and welcome screen implemented

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:uggiso/widgets/IntroLandingScreen.dart';
+import 'package:uggiso/widgets/SignUpScreen.dart';
 import 'package:uggiso/widgets/splash.dart';
 
 class AppRoutes {
@@ -6,13 +8,9 @@ class AppRoutes {
 
   static const String introLanding = '/introLanding';
 
-  static const String loginOrSignupScreen = '/login_or_signup_screen';
-
   static const String signupScreen = '/signup_screen';
 
-  static const String loginScreen = '/login_screen';
-
-  static const String forgotPasswordScreen = '/forgot_password_screen';
+  static const String verifyOtp = '/verify_otp';
 
   static const String homeScreen = '/home_screen';
 
@@ -22,10 +20,25 @@ class AppRoutes {
 
   static const String appNavigationScreen = '/app_navigation_screen';
 
-
-
-  static get routes => {
-    initialRoute: const SplashScreen(),
-    introLanding : const IntroLandingScreen()
-  };
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case initialRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case introLanding:
+        return MaterialPageRoute(builder: (_) => const IntroLandingScreen());
+      case signupScreen:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      case verifyOtp:
+        return MaterialPageRoute(builder: (_) => const IntroLandingScreen());
+      default:
+      // If there is no such named route in the switch statement, e.g. /randomRoute
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          );
+        });
+    }
+  }
 }
