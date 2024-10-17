2847878152ba252ef0ca22659b6108277d780af6
Tilakrk23 <tilak@innoventes.co>
2024-10-17 20:23:31 +0530

    Small changes

--- lib/Widgets/splash.dart
+++ lib/Widgets/splash.dart
@@ -142,7 +142,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     }
     else{
-      Navigator.popAndPushNamed(context, AppRoutes.introLanding);
+      Navigator.popAndPushNamed(context, AppRoutes.homeScreen);
 
     }
 
